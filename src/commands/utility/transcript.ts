ffbab6c0aedf9a5a71e8ec150af4d2367ab241fc
Viish <104511692+ViishSensei@users.noreply.github.com>
2025-03-11 11:29:15 +0530

    gg

--- src/commands/utility/transcript.ts
+++ src/commands/utility/transcript.ts
@@ -1,14 +1,15 @@
 /** @format */
 
+import transcript from 'discord-selfbot-transcripts';
 import type { Message, TextChannel } from 'discord.js-selfbot-v13';
 import type Viish from '../../base/Client.js';
-import transcript from 'discord-selfbot-transcripts';
 
 export default {
   name: 'transcript',
   run: async (_client: Viish, message: Message, _args: string[]) => {
     await message.reply({
       files: [
+        // @ts-expect-error
         await transcript.createTranscript(message.channel, {
           fileName: `${(message.channel as TextChannel).name}.html`
         })
